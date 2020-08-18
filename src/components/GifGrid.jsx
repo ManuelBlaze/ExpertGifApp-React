f92ce6feae28617c895d1cbee71a04eded3488fe
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-08-18 17:51:18 -0500

    Custom Hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import GifGridItem from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-        getGifs( category )
-            .then( setImages );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    const { data: images, loading } = useFetchGifs( category );
 
     return (
         <div className='mb-5'>
             <h3> { category } </h3>
+
+            { loading && <span className="badge badge-pill badge-info text-center">Loading</span> }
+
             <div className="row">
                 {
                     images.map( img => (
